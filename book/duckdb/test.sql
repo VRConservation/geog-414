629b7f478f67b50364a39702dffdc4a4941656c7
VRConservation <61313347+VRConservation@users.noreply.github.com>
2023-10-26 07:07:54 -0500

    sql changes and notes

---
+++ book/duckdb/test.sql
@@ -0,0 +1,19 @@
+SELECT * FROM duckdb_extensions();
+CREATE TABLE cities AS SELECT * FROM 'https://open.gishub.org/data/duckdb/cities.csv';
+CREATE TABLE countries AS SELECT * FROM 'https://open.gishub.org/data/duckdb/countries.csv';
+FROM cities;
+FROM countries
+SELECT * FROM cities LIMIT 10;
+SELECT name, country FROM cities LIMIT 10;
+SELECT COUNT(*) FROM cities;
+SELECT MAX(population) FROM cities;
+SELECT * FROM cities ORDER BY country LIMIT 5;
+SELECT * FROM cities WHERE country='USA';
+SELECT * FROM cities WHERE country='USA' AND population>1000000;
+CREATE TABLE cities (
+    name VARCHAR,
+    lat DECIMAL,
+    lon DECIMAL,
+);
+SHOW TABLES;
+DESCRIBE cities;
